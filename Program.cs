c1c4d1844f4ba6374201660912515c870298d36f
Math3uz7 <matheuspprudencio@gmail.com>
2024-09-15 22:38:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.AccessControl;
+
+
+string opcao;
+
+while (true)
+{
+    Console.WriteLine("Digite sua opcao");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar de cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+        case "4":
+            Console.WriteLine("Encerrar");
+            Environment.Exit(0);
+            break;
+
+        default:
+            Console.WriteLine("Opcao invalida");
+            break;
+    }
+}
+
+
