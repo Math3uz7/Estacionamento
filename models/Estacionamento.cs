7b7e700888d9ad316601253d7e6e5b3753d890bf
Math3uz7 <matheuspprudencio@gmail.com>
2024-09-16 21:18:03 -0300

    update_final

--- models/Estacionamento.cs
+++ models/Estacionamento.cs
@@ -31,14 +31,14 @@ namespace projeto_C_1.models
                 switch (opcao)
                 {
                     case "1":
-                        Console.WriteLine("Digite a placa so seu veiculo");
+                        Console.WriteLine("Digite a placa do seu veiculo");
                         placa = Console.ReadLine();
                         carro.Add(placa);
                         Console.WriteLine("Precione qualquer tecla para continuar");
                         Console.ReadLine();
                         break;
                     case "2":
-                        Console.WriteLine("Os veiculos estacionados sao: ");
+                        Console.WriteLine("Os veiculos estacionados são: ");
                         int contador = 0;
                         foreach (string valor in carro)
                         {
@@ -49,11 +49,31 @@ namespace projeto_C_1.models
                         Console.ReadLine();
                         break;
                     case "3":
-                        carro.Remove(placa);
-                        Console.WriteLine($"Itens da lista: {carro.Count}");
-                        Console.WriteLine("Item removido");
-                        Console.WriteLine("Precione qualquer tecla para continuar");
-                        Console.ReadLine();
+                        Console.WriteLine("Digite a placa do veiculo para remover:");
+                        string removePlaca = Console.ReadLine();
+
+                        if (removePlaca == placa)
+                        {
+                            int converte1 = Convert.ToInt32(hora);
+                            int converte2 = Convert.ToInt32(inicial);
+
+                            int soma = converte1 * converte2;
+
+                            Console.WriteLine($"O valor a ser pago é: {soma}");
+
+                            carro.Remove(placa);
+
+                            Console.WriteLine($"Veiculos estacionados: {carro.Count}");
+                            Console.WriteLine("Veiculo removido");
+                            Console.WriteLine("Precione qualquer tecla para continuar");
+                            Console.ReadLine();
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Placa não encontrada");
+
+                        }
                         break;
                     case "4":
                         Environment.Exit(0);
